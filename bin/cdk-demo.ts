7ced76ed5266a9f5cac53f2d4c27efe2821f59d4
MitomiTaisei <mton.marvel@gmail.com>
2022-10-05 19:45:12 +0900

    add AcountId and reagion

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -17,5 +17,7 @@ new CdkDemoStack(app, 'CdkDemoStack', {
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
 
+  env: { account: '948469033258', region: 'eu-west-1' },
+
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
