f476fe5f1cb73d6ed1c750d5f7c2d2b635667775
MitomiTaisei <mton.marvel@gmail.com>
2022-10-05 19:46:49 +0900

    add demo-vpcの設定

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+import { Vpc, SubnetType } from 'aws-cdk-lib/aws-ec2';
+
 export class CdkDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -12,5 +14,14 @@ export class CdkDemoStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const vpc = new Vpc(this, 'MainVpc',{
+    maxAzs: 2,
+      subnetConfiguration:  [{
+        cidrMask: 24,
+        name: 'public-subnet',
+        subnetType: SubnetType.PUBLIC
+      }]
+    });
   }
 }
